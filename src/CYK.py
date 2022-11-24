1e42bd02c571705aa48be24adc1dce5e82b352dd
farizkik <fariski.mail@gmail.com>
2022-11-24 13:31:46 +0700

    optimize cyk

--- src/CYK.py
+++ src/CYK.py
@@ -1,5 +1,6 @@
 productions = []
 
+
 def readFile(fileCNF):
     file = open(fileCNF, 'r')
     CNFProductions = file.readlines()
@@ -14,43 +15,47 @@ def readFile(fileCNF):
         # array of terminal/non-terminals
         RHS = CNFProductions[i].split(' -> ')[1].split(' | ')
         productions.append([LHS, RHS])
-        
+
+
 readFile('grammar/CNF.txt')
+
 
 def parse(token):
     tabel = [
-            [ [] for _ in range(len(token)-i) ]
-            for i in range(len(token))
-        ]
-    
+            [[] for _ in range(len(token)-i)]
+        for i in range(len(token))
+    ]
+
     # isi tabel paling bawah
     for i in range(len(token)):
         for j in range(len(productions)):
             if token[i] in productions[j][1]:
                 tabel[0][i].append(productions[j][0])
-                
+
     # traversal dari tinggi 1 hingga teratas
-    for i in range(1,len(token)):
+    for i in range(1, len(token)):
         # j looping semua elemen pada ketinggian i
         for j in range(len(token)-i):
-            
+
             for k in range(i):
                 # coba semua kombinasi
                 for l in range(len(tabel[k][j])):
                     for r in range(len(tabel[i-k-1][j+k+1])):
-                        check=tabel[k][j][l] + " " + tabel[i-k-1][j+k+1][r]
+                        check = tabel[k][j][l] + " " + tabel[i-k-1][j+k+1][r]
                         for m in range(len(productions)):
                             if check in productions[m][1]:
+                                if (productions[m][0] in tabel[i][j]):
+                                    continue
                                 tabel[i][j].append(productions[m][0])
 
-    #print buat debug
-    #for n in range(len(token)):
-    #    for i in range(len(token)-n):
-    #        print(tabel[n][i])
-    #    print('next')
-    #print('here')
-    
+    '''
+    # print buat debug
+    for n in range(len(token)):
+        for i in range(len(token)-n):
+            print(tabel[n][i])
+        print('next')
+    print('here')
+    '''
     # mengeluarkan true jika S berada di kotak paling atas
     print('S' in tabel[len(token)-1][0])
     return 'S' in tabel[len(token)-1][0]
-                                
--- test/input4.js
+++ test/input4.js
@@ -6,7 +6,3 @@ else if (x == 3)
 {
 	let b = 4;
 }
-else
-{
-	let c = 5;
-}
